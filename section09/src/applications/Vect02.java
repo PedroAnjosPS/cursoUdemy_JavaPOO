e96a587c152a0e03e9d0cf5791217e66f0747493
PedroAnjosPS <phpsanches@gmail.com>
2025-10-07 15:24:13 -0300

    Exercício sobre Vetor - parte 2

---
+++ section09/src/applications/Vect02.java
@@ -0,0 +1,53 @@
+package applications;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product02;
+
+public class Vect02 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter a number: ");
+		int n = sc.nextInt();
+		
+		System.out.println();
+		
+		Product02[] vect = new Product02[n];
+		
+		for(int i = 0; i < n; i++) {
+			sc.nextLine();
+			
+			System.out.print("Enter a name: ");
+			String name = sc.nextLine();
+			
+			System.out.print("Enter a price: $ ");
+			double price = sc.nextDouble();
+			
+			System.out.println();
+			
+			vect[i] = new Product02(name, price);
+		}
+		
+		double sum = 0.0;
+		
+		for(int i = 0; i < n; i++) {
+			sum += vect[i].getPrice();
+		}
+		
+		double avg = sum / n;
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println(vect[i]);
+		} 
+		
+		System.out.printf("%nAVERAGE PRICE = $ %.2f", avg);
+
+		sc.close();
+	}
+
+}
